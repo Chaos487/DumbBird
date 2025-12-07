646b98e1c7d2fb813bffc6c576062a9c4bdb6a0b
kevin <20010729487lka@gmail.com>
2025-12-07 14:30:06 +0000

    GameOver Logic

--- Assets/Script/LogicManager.cs
+++ Assets/Script/LogicManager.cs
@@ -1,14 +1,28 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicManager : MonoBehaviour
 {
     public int playerScore;
     public Text scoreText;
+    public GameObject gameOverScreen;
 
-    public void addScore()
+
+    public void addScore(int scoreAdded)
     {
-        playerScore = playerScore +1;
+        playerScore = playerScore + scoreAdded;
         scoreText.text = playerScore.ToString();
     }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+
 }
